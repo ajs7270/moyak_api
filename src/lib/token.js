a2880b05c8775dbf734b3c57be0b9830d85082eb
ohyaewon <aa9919@cau.ac.kr>
2019-02-01 06:41:58 +0900

    add package-json

--- src/lib/token.js
+++ src/lib/token.js
@@ -23,16 +23,7 @@ function generateToken(payload) {
     );
 };
 
-function decodeToken(token) {
-    return new Promise(
-        (resolve, reject) => {
-            jwt.verify(token, jwtSecret, (error, decoded) => {
-                if(error) reject(error);
-                resolve(decoded);
-            });
-        }
-    );
-}
+
 
 function decodeToken(token) {
     return new Promise(
